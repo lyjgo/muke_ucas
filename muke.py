78d5c9c58d6529328078167a375d139714904469
lyjgo <956010978@qq.com>
2023-03-08 20:05:36 +0800

    second commit

--- muke.py
+++ muke.py
@@ -5,6 +5,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+#
+# 请修改以下两行，详情见README.md
+#
+DRIVER_PATH = '文件路径\chromedriver.exe'
+muke_html = ""    # 复制的链接粘贴在此
 
 def is_alert_present(driver):
     try:
@@ -36,14 +41,16 @@ def gogogo(driver, check_flag):
 def watch_video(driver, video_index, inner_iframe, check_flag):
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[4]/div/button")))
     driver.find_element("xpath", "/html/body/div[2]/div[4]/div/button").click()
-    driver.find_element("xpath", "/html/body/div[2]/div[4]/div/div[6]/div[1]/button").click()
-    time.sleep(0.2)
-    driver.find_element("xpath", "/html/body/div[2]/div[4]/div/div[6]/div[1]/button").click()
-    time.sleep(0.2)
-    driver.find_element("xpath", "/html/body/div[2]/div[4]/div/div[6]/div[1]/button").click()
-    time.sleep(0.2)
+    if check_flag == 1:
+        driver.find_element("xpath", "/html/body/div[2]/div[4]/div/div[6]/div[1]/button").click()
+        time.sleep(0.2)
+        driver.find_element("xpath", "/html/body/div[2]/div[4]/div/div[6]/div[1]/button").click()
+        time.sleep(0.2)
+        driver.find_element("xpath", "/html/body/div[2]/div[4]/div/div[6]/div[1]/button").click()
+        time.sleep(0.2)
     print(video_index)
     driver.switch_to.parent_frame()
+    replay_index = 1
     while driver.find_elements("xpath", "//*[@id=\"ext-gen1049\"]")[video_index].get_attribute(
             "class") != "ans-attach-ct ans-job-finished":  # 观看状态
         print("watching now...")
@@ -51,10 +58,19 @@ def watch_video(driver, video_index, inner_iframe, check_flag):
             driver.switch_to.frame(inner_iframe[video_index])
             i = 1
             while is_alert_present(driver):
-                driver.find_element("xpath", "//*[@class=\"tkTopic_con tkScroll\"]/div/ul/li[{_i}]/label".format(_i=i)).click()
-                i = i + 1
+                if replay_index == 1:
+                    driver.find_element("xpath", "//*[@class=\"tkTopic_con tkScroll\"]/div/ul/li[{_i}]/label".format(_i=i)).click()
+                else:
+                    driver.find_element("xpath", "//*[@class=\"tkTopic_con tkScroll\"]/div/ul/li[{_i}]/label".format(_i=replay_index)).click()
                 time.sleep(0.2)
-                driver.find_element("xpath", "//*[@id=\"videoquiz-submit\"]").click()
+                try:
+                    driver.find_element("xpath", "//*[@id=\"videoquiz-submit\"]").click()
+                    i = i + 1
+                    time.sleep(0.2)
+                except:
+                    driver.find_element("xpath", "//*[@id=\"videoquiz-continue\"]").click()
+                    replay_index = i
+                    time.sleep(0.2)
                 time.sleep(0.2)
             driver.switch_to.parent_frame()
         time.sleep(1)
@@ -62,20 +78,18 @@ def watch_video(driver, video_index, inner_iframe, check_flag):
 def watch_pdf(driver):
     innest_iframe = driver.find_elements(By.TAG_NAME, "iframe")
     driver.switch_to.frame(innest_iframe[0])
-    for page in range(1, 1000):
-        try:
-            img = driver.find_element("xpath", "/html/body/div[1]/img[{_i}]".format(_i=page))
-            driver.execute_script("arguments[0].scrollIntoView(false);", img)
-        except:
-            driver.switch_to.parent_frame()
-            driver.switch_to.parent_frame()
-            break
+    pages = driver.find_elements("xpath", "//*[@class=\"pageNum\"]")
+    page_num = len(pages)
+    for i in range(page_num):
+            page = pages[i]
+            driver.execute_script("arguments[0].scrollIntoView(false);", page)
+    driver.switch_to.parent_frame()
+    driver.switch_to.parent_frame()
 
 options = Options()
 options.headless = False
 # option = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-DRIVER_PATH = '文件路径\chromedriver.exe'
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 allhandles=driver.window_handles
 nowhandle = driver.current_window_handle
@@ -85,7 +99,8 @@ time.sleep(0.5)
 driver.get("http://i.mooc.ecourse.ucas.ac.cn/space/index")
 driver.maximize_window()
 time.sleep(10)
-driver.execute_script('window.open("复制的链接")')     #详情见README.md
+driver.execute_script('window.open(_html)'.format(_html = muke_html))
+
 time.sleep(1)
 driver.switch_to.window(driver.window_handles[1])
 des = ''
@@ -96,7 +111,9 @@ for capter in range(1,11):
         try:
             xpath = "/html/body/div[5]/div[1]/div[2]/div[3]/div[{_capter}]/div[{_i}]/h3/a/span[2]/em".format(_capter = capter, _i = i)
             state = driver.find_element(By.XPATH, xpath).get_attribute("class")
-            if state == "orange":
+            content_xpath = "/html/body/div[5]/div[1]/div[2]/div[3]/div[{_capter}]/div[{_i}]/h3/a/span[3]".format(_capter = capter, _i = i)
+            content = driver.find_element(By.XPATH, content_xpath).get_attribute("title")
+            if state == "orange" and ("Quiz" not in content):
                 des = "/html/body/div[5]/div[1]/div[2]/div[3]/div[{_capter}]/div[{_i}]/h3/a/span[3]".format(_capter = capter, _i = i)
                 check_flag = 0 if i == 1 else 1
                 break
@@ -116,6 +133,12 @@ for capter in range(1,11):
             state = driver.find_element(By.XPATH, xpath).get_attribute("class")
             if state == "roundpointStudent  orange01 a002 jobCount":
                 des = "/html/body/div[4]/div[1]/div[2]/div[1]/div/div[{_capter}]/div[{_i}]/h4/a".format(_capter = capter, _i = i)
+                content = driver.find_element("xpath",
+                                              "/html/body/div[4]/div[1]/div[2]/div[1]/div/div[{_capter}]/div[{_i}]/h4/a/span".format(
+                                                  _capter=capter, _i=i))
+                print(content.text)
+                if "Quiz" in content.text:
+                    continue
                 driver.find_element("xpath", des).click()
                 time.sleep(2)
                 gogogo(driver, check_flag)
@@ -123,5 +146,4 @@ for capter in range(1,11):
                 continue
         except:
             break
-
 
